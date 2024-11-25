87029413a88fc1906b42fc3e63bd9f9f61cddf14
Anushka Sudhakar Karade <162126303+anushkaskarade@users.noreply.github.com>
2024-11-25 13:26:53 -0500

    Create ArmstrongNum.c

---
+++ ArmstrongNum.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to count the number of digits in a given number
+int countDigits(int number) {
+    int count = 0;
+    while (number != 0) {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
+
+// Function to check if a number is an Armstrong number
+int isArmstrong(int number) {
+    int originalNumber = number;
+    int numDigits = countDigits(number);
+    int sum = 0;
+
+    while (number != 0) {
+        int digit = number % 10;
+        sum += pow(digit, numDigits);
+        number /= 10;
+    }
+
+    return sum == originalNumber;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isArmstrong(num))
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
